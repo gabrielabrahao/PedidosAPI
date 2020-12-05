63ede88006c33d9d23e9da794f318a09aab3aba8
gabrielabrahao <gabriel.abrahao95@gmail.com>
2020-12-05 00:17:54 -0300

    Configuração do JPA para conexão com Postgres e criado classe de Categorias

--- src/main/java/com/gabrielabrahao/PedidosApi/domain/Categoria.java
+++ src/main/java/com/gabrielabrahao/PedidosApi/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.gabrielabrahao.PedidosApi.domain;
 
-import java.io.Serializable;
+import javax.persistence.Entity;
 
-public class Categoria implements Serializable{
+import com.sun.istack.NotNull;
+
+@Entity
+public class Categoria extends AbstractEntity<Integer>{
 	private static final long serialVersionUID = 1L;
 	
 	
-	private Integer id;
+    @NotNull
 	private String nome;
 	
 	public Categoria() {
@@ -15,17 +18,10 @@ public class Categoria implements Serializable{
 
 	public Categoria(Integer id, String nome) {
 		super();
-		this.id = id;
+	    Categoria.this.setId(id);
 		this.nome = nome;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
 	public String getNome() {
 		return nome;
@@ -35,36 +31,6 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		return true;
-	}
 	
 	
 
