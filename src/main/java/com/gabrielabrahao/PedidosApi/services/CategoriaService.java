f4911f0fe68ebda318e326600fcd25494ec9e967
gabrielabrahao <gabriel.abrahao95@gmail.com>
2020-12-11 21:15:36 -0300

    Endpoint Delete para Categoria

--- src/main/java/com/gabrielabrahao/PedidosApi/services/CategoriaService.java
+++ src/main/java/com/gabrielabrahao/PedidosApi/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.gabrielabrahao.PedidosApi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.gabrielabrahao.PedidosApi.domain.Categoria;
 import com.gabrielabrahao.PedidosApi.repositories.CategoriaRepository;
+import com.gabrielabrahao.PedidosApi.services.exceptions.DataIntegrityException;
 import com.gabrielabrahao.PedidosApi.services.exceptions.ObjectNotFoundException;
 
 
@@ -31,6 +33,18 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		//DataIntegrityViolationException
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos vinculados");
+		}
+		
+	
 	}
 
 }
